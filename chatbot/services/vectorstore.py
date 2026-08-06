97c59b06c2911e141bbb5b3a260c5863b8952929
vibhor <vibhorg62@gmail.com>
2026-08-06 21:27:32 +0530

    Use FastEmbed

--- chatbot/services/vectorstore.py
+++ chatbot/services/vectorstore.py
@@ -2,13 +2,10 @@ import os
 import sys
 
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import FastEmbedEmbeddings
 
 
-MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
-
-# Lazy Loading
 embeddings = None
 
 
@@ -18,36 +15,28 @@ def get_embeddings():
 
     if embeddings is None:
 
-        print("Loading Embedding Model...", file=sys.stderr)
+        print("Loading FastEmbed...", file=sys.stderr)
 
-        embeddings = HuggingFaceEmbeddings(
-            model_name=MODEL_NAME
-        )
+        embeddings = FastEmbedEmbeddings()
 
-        print("Embedding Model Loaded", file=sys.stderr)
+        print("FastEmbed Loaded", file=sys.stderr)
 
     return embeddings
 
 
 def create_retriever(text, video_id):
 
-    print("A. create_retriever()", file=sys.stderr)
-
-    # Create parent directory
-    os.makedirs("vectorstore", exist_ok=True)
+    print("Creating Retriever...", file=sys.stderr)
 
     folder_path = os.path.join("vectorstore", video_id)
 
-    print(f"VectorStore Path : {folder_path}", file=sys.stderr)
+    os.makedirs("vectorstore", exist_ok=True)
 
-    # Load embedding model only when needed
     embedding_model = get_embeddings()
-
-    # ---------------- LOAD ---------------- #
 
     if os.path.exists(folder_path):
 
-        print("B. Loading Existing VectorStore...", file=sys.stderr)
+        print("Loading Existing VectorStore...", file=sys.stderr)
 
         vector_store = FAISS.load_local(
             folder_path,
@@ -55,13 +44,7 @@ def create_retriever(text, video_id):
             allow_dangerous_deserialization=True
         )
 
-        print("C. Existing VectorStore Loaded", file=sys.stderr)
-
-    # ---------------- CREATE ---------------- #
-
     else:
-
-        print("D. Splitting Transcript...", file=sys.stderr)
 
         splitter = RecursiveCharacterTextSplitter(
             chunk_size=1000,
@@ -70,30 +53,18 @@ def create_retriever(text, video_id):
 
         docs = splitter.create_documents([text])
 
-        print(f"E. Total Chunks : {len(docs)}", file=sys.stderr)
-
-        print("F. Creating FAISS...", file=sys.stderr)
+        print(f"Chunks : {len(docs)}", file=sys.stderr)
 
         vector_store = FAISS.from_documents(
             docs,
             embedding_model
         )
 
-        print("G. FAISS Created", file=sys.stderr)
-
-        print("H. Saving VectorStore...", file=sys.stderr)
-
         vector_store.save_local(folder_path)
 
-        print("I. VectorStore Saved", file=sys.stderr)
+    print("Retriever Ready", file=sys.stderr)
 
-    # ---------------- RETRIEVER ---------------- #
-
-    retriever = vector_store.as_retriever(
+    return vector_store.as_retriever(
         search_type="similarity",
-        search_kwargs={"k": 4}
+        search_kwargs={"k":4}
     )
-
-    print("J. Retriever Ready", file=sys.stderr)
-
-    return retriever
